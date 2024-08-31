b3398fb6a6557593b2cfb8ea1baa25c0b9b04c7a
Taras <tarasdemchenko19042007@gmail.com>
2024-08-31 13:37:48 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './global.css'
+
+const domNode = document.getElementById('root')!
+const root = createRoot(domNode)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
