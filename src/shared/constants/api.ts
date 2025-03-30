06ed59c2abe4d9616d26855e7342a1cfc0fe669a
Taras <tarasdemchenko19042007@gmail.com>
2025-03-31 00:06:12 +0300

    fix backend api url

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,5 +1,5 @@
 export default {
-    backendUrl: "https://starge.xyz/api/",
+    backendUrl: "https://api.starge.xyz/",
     tonapiUrl: "https://tonapi.io/v2/",
     tonapiKey: "AEJ6J7NK7R2ECUQAAAAO3VKDETCLMFPQ2IP67K7C7ZXOBR6AV7YGOVAKD6MODZEILD4SNWA"
 }
